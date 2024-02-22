737a9841e7585b7c75fecb888b0aa2140b89b469
Jackie-of-all-trades24 <jackiej@gatech.edu>
2024-02-22 18:35:26 -0500

    [Trello-Card-#31] [feat]: [Account Creation Functionality]
    Updated Account Creation Screen to match UI of Login Screen (Completed)
    ViewModel and Sign in Functionality (Work In Progress)

---
+++ app/src/main/java/com/example/cs2340a_team13/viewModels/CreateAccountViewModel.java
@@ -0,0 +1,10 @@
+package com.example.cs2340a_team13.viewModels;
+
+public class CreateAccountViewModel {
+    private static CreateAccountViewModel instance;
+
+    public CreateAccountViewModel() {
+
+    }
+    public static synchronized CreateAccountViewModel
+}
--- app/src/main/java/com/example/cs2340a_team13/views/AccountCreationScreen.java
+++ app/src/main/java/com/example/cs2340a_team13/views/AccountCreationScreen.java
@@ -3,14 +3,20 @@ package com.example.cs2340a_team13.views;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.EditText;
 
 import com.example.cs2340a_team13.R;
 
 public class AccountCreationScreen extends AppCompatActivity {
-
+    private AccountCreationViewModel viewModel;
+    private EditText usernameEditText;
+    private EditText passwordEditText;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_account_creation_screen);
     }
+
+
+
 }
