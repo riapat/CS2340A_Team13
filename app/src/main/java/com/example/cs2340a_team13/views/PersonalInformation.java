f439e0127e0d04ad9c0ad438832b8525c511f4d1
agnessc9 <alwaysagnes9@gmail.com>
2024-02-29 10:53:23 -0500

    added the Personal Information page view and layout resource file

---
+++ app/src/main/java/com/example/cs2340a_team13/views/PersonalInformation.java
@@ -0,0 +1,15 @@
+package com.example.cs2340a_team13.views;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.cs2340a_team13.R;
+
+public class PersonalInformation extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_personal_info_screen);
+    }
+}
