7ec3a9e05c086374d917a8e0918f2d13fd5097df
Vishruth <vishruth.anand@gmail.com>
2024-03-04 13:38:27 -0500

    Created a meal model with meal name and calorie count attributes

---
+++ app/src/main/java/com/example/cs2340a_team13/model/Meal.java
@@ -0,0 +1,35 @@
+package com.example.cs2340a_team13.model;
+
+public class Meal {
+    // Attributes
+    private String name;
+    private int calorieCount;
+
+    // Constructors
+    public Meal() {
+        this.name = "";
+        this.calorieCount = 0;
+    }
+
+    public Meal(String name, int calorieCount) {
+        this.name = name;
+        this.calorieCount = calorieCount;
+    }
+
+    // Getters and setters
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getCalorieCount() {
+        return calorieCount;
+    }
+
+    public void setCalorieCount(int calorieCount) {
+        this.calorieCount = calorieCount;
+    }
+}
