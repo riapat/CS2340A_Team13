dfc2356c75a4dae150689733411c9604f663dfc0
agnessc9 <alwaysagnes9@gmail.com>
2024-03-08 00:24:27 -0500

    created UserModel with personal information attributes and allowed user to edit these attributes through the personal information screen

--- app/src/main/java/com/example/cs2340a_team13/model/User.java
+++ app/src/main/java/com/example/cs2340a_team13/model/User.java
@@ -1,9 +1,16 @@
 package com.example.cs2340a_team13.model;
 
 public class User {
-
     private String username;
     private String password;
+
+    private int height;
+
+    private int weight;
+
+    private int age;
+
+    private String gender;
 
     public User() {
 
@@ -12,6 +19,10 @@ public class User {
     public User(String username, String password) {
         this.username = username;
         this.password = password;
+        this.height = 0;
+        this.weight = 0;
+        this.age = 0;
+        this.gender = "";
     }
 
     public String getUsername() {
@@ -22,6 +33,22 @@ public class User {
         return password;
     }
 
+    public int getHeight() {
+        return height;
+    }
+
+    public int getWeight() {
+        return weight;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
     public void setUsername(String username) {
         this.username = username;
     }
@@ -30,8 +57,28 @@ public class User {
         this.password = password;
     }
 
+    public void setHeight(int height) {
+        this.height = height;
+    }
+
+    public void setWeight(int weight) {
+        this.weight = weight;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
     public void setUser(User user) {
         this.username = user.getUsername();
         this.password = user.getPassword();
+        this.height = user.getHeight();
+        this.weight = user.getWeight();
+        this.age = user.getAge();
+        this.gender = user.getGender();
     }
 }
