e37a81bb27e3af744fc9fc0457a15b019015070c
riapat <ria2004.patel@gmail.com>
2024-03-11 23:50:32 -0400

    feat: added tests and fixed Agnes' test with the new callback and forced synchronous nature of Firebase methods

--- app/src/test/java/com/example/cs2340a_team13/RiaPatelTests.java
+++ app/src/test/java/com/example/cs2340a_team13/RiaPatelTests.java
@@ -3,18 +3,36 @@ package com.example.cs2340a_team13;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
+import com.example.cs2340a_team13.model.Meal;
 import com.example.cs2340a_team13.model.User;
+import java.util.Date;
 
 public class RiaPatelTests {
-// Testing Singleton DatabaseAccess class
+
+// Testing User logged Meals cleared
     @Test
-    public void testSingletonProperty() {
-        DatabaseAccess databaseAccess1 = DatabaseAccess.getInstance();
-        DatabaseAccess databaseAccess2 = DatabaseAccess.getInstance();
-        assertSame("Both instances should be the same",databaseAccess1, databaseAccess2);
+    public void testClearLoggedMeals() {
+        User user = new User("test", "testing");
+        user.addMeal(new Meal("Pizza", 300));
+        user.addMeal(new Meal("Burger", 500));
+        user.clearLoggedMeals();
+        user.addMeal(new Meal("", 0));
+        assertEquals(1, user.getMeals().size());
+        user.getMeals().get(0).setName("Rice");
+        assertEquals("Rice", user.getMeals().get(0).getMealName());
+
+        User user2 = new User("test2", "testing2");
+        user2.addMeal(new Meal("Coca Cola", 150));
+        assertNotEquals("Rice", user2.getMeals().get(0).getMealName());
     }
 
-// Testing writeToUserDB method in DatabaseAccess class
-    @Test
 
+    @Test
+    public void testEmptyDateLoggedMeal() {
+        User user = new User("test", "testing");
+        user.addMeal(new Meal("Pizza", 300));
+        //Initialized Date
+        Date expectedDate = new Date();
+        assertEquals(expectedDate,user.getMeals().get(0).getDate());
+    }
 }
