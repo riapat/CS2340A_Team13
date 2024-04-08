450c3eab0888dd5f1bd959388bcd74c374ccadcd
Jackie-of-all-trades24 <jackiej@gatech.edu>
2024-04-08 13:36:29 -0400

    Adding Shoppinglist items to screen

--- app/src/main/java/com/example/cs2340a_team13/views/ShoppingListScreen.java
+++ app/src/main/java/com/example/cs2340a_team13/views/ShoppingListScreen.java
@@ -6,10 +6,17 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
 
 import com.example.cs2340a_team13.R;
+import com.example.cs2340a_team13.model.Ingredient;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
 
 public class ShoppingListScreen extends AppCompatActivity {
+    private List<CheckBox> shoppingListItems;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -65,4 +72,32 @@ public class ShoppingListScreen extends AppCompatActivity {
         });
     }
     //input shopping screen here + place text header
+
+    private void initializeCart(List<CheckBox> shoppingListButtons, List<Ingredient> shoppingCart){
+        shoppingListButtons = new LinkedList<CheckBox>();
+        for (Ingredient cartItem:shoppingList){
+            CheckBox item = new CheckBox(this);
+            String name = cartItem.getIngredientName() + "\n QTY: " + cartItem.getQuantity();
+            item.setText(name);
+            item.setChecked(false);
+            shoppingListButtons.add(item);
+        }
+    }
+
+    private List<Ingredient> getSelectedIngredients(List<CheckBox> shoppingListButtons,
+                                                    List<Ingredient> shoppingCart){
+        List<Ingredient> selectedItems = new ArrayList<Ingredient>();
+        for (CheckBox item:shoppingListButtons) {
+            if(item.isChecked()){
+                String[] itemInfo = item.getText().toString().split("\\R");
+                for (Ingredient shoppingCartItem: shoppingCart) {
+                    if (shoppingCartItem.getIngredientName().equals(itemInfo[0]){
+                        selectedItems.add(shoppingCartItem);
+                        break;
+                    }
+                }
+            }
+        }
+        return selectedItems;
+    }
 }
